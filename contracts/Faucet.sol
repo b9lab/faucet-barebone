80e9495561e27be25640de2ea65d2ea352075c55
Xavier Lepretre <xavier.lepretre@gmail.com>
2016-12-12 13:05:05 +0000

    Formatting.

--- contracts/Faucet.sol
+++ contracts/Faucet.sol
@@ -9,29 +9,29 @@ contract Faucet {
         sendAmount = 1000000000000000000;
     }
 
-	function getBalance() returns (uint) {
-	     return address(this).balance;
-	}
+    function getBalance() returns (uint) {
+         return address(this).balance;
+    }
 
-	function getWei() returns (bool) {
+    function getWei() returns (bool) {
         msg.sender.send(sendAmount);
         return true;
-	}
+    }
 
-	function sendWei(address toWhom) payable returns (bool) {
+    function sendWei(address toWhom) payable returns (bool) {
         toWhom.send(sendAmount);
         return true;
-	}
+    }
 
-	function getSendAmount() returns (uint256) {
-	    return sendAmount;
-	}
+    function getSendAmount() returns (uint256) {
+        return sendAmount;
+    }
 
-	function killMe() {
-	    if (msg.sender == owner) {
-	        suicide(owner);
-	    }
-	}
+    function killMe() {
+        if (msg.sender == owner) {
+            suicide(owner);
+        }
+    }
 
-	function () payable {}
+    function () payable {}
 }
