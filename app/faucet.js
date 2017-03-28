c3dedbc759eed125fd6d5b99652cd2665d81f38e
Xavier Lepretre <xavier.lepretre@gmail.com>
2017-03-28 11:10:22 +0100

    Made all calls asynchronous.

--- app/faucet.js
+++ app/faucet.js
@@ -4,31 +4,79 @@ if (typeof web3 !== 'undefined') {
   // set the provider you want from Web3.providers
   web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"));
 }
-console.log("Coinbase: " + web3.eth.coinbase);
+web3.eth.getCoinbase(function(err, coinbase) {
+	if (err) {
+		console.log(err);
+	} else {
+		console.log("Coinbase: " + coinbase);
+	}
+});
 
 // Your deployed address changes every time you deploy.
 var faucetAddress = "0xf8767ad850761e0682fbb4949a5c51f9c364311b";
 faucetInstance = web3.eth.contract(faucetCompiled["<stdin>:Faucet"].info.abiDefinition).at(faucetAddress);
 
 // Query eth for balance
-console.log("Contract balance: " + web3.eth.getBalance(faucetAddress));
+web3.eth.getBalance(faucetAddress, function(err, balance) {
+	if (err) {
+		console.log(err);
+	} else {
+		console.log("Contract balance: " + balance);
+	}
+});
 
 // Query the contract directly
-console.log("Faucet balance: " + faucetInstance.getBalance.call());
+faucetInstance.getBalance.call(function(err, balance) {
+	if (err) {
+		console.log(err);
+	} else {
+		console.log("Faucet balance: " + balance);
+	}
+});
 
 function topUp() {
-	var txn = web3.eth.sendTransaction({ 
-		from: web3.eth.coinbase, 
-		to: faucetAddress,
-		value: web3.toWei(1, "ether") 
+	web3.eth.getCoinbase(function(err, coinbase) {
+		if (err) {
+			console.log(err);
+		} else {
+			web3.eth.sendTransaction({ 
+				from: coinbase, 
+				to: faucetAddress,
+				value: web3.toWei(1, "ether") 
+			}, function(err, txn) {
+				if (err) {
+					console.log(err);
+				} else {
+					console.log("topUp txn: " + txn);
+				}
+			});
+		}
 	});
-	console.log("topUp txn: " + txn);
 }
 
-var targetAccount = web3.eth.accounts[1];
+
 function sendWei() {
-	var txn = faucetInstance.sendWei(
-		targetAccount,
-		{from: web3.eth.coinbase});
-	console.log("sendWei txn: " + txn);
+	web3.eth.getCoinbase(function(err, coinbase) {
+		if (err) {
+			console.log(err);
+		} else {
+			web3.eth.getAccounts(function(err, accounts) {
+				if (err) {
+					console.log(err);
+				} else {
+					var targetAccount = accounts[1];		
+					faucetInstance.sendWei(
+						targetAccount,
+						{from: coinbase},
+						function(err, txn) {
+							if (err) {
+								console.log(err);
+							} else {
+								console.log("sendWei txn: " + txn);
+							}
+						});
+				}
+			});
+		}
+	});
 }
