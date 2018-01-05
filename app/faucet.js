90ecc11da053e3c437e02bd46991967d44fb545e
Xavier Lepretre <xavier.lepretre@gmail.com>
2018-01-05 16:33:25 +0000

    Changed indentation to spaces.

--- app/faucet.js
+++ app/faucet.js
@@ -1,16 +1,16 @@
 if (typeof web3 !== 'undefined') {
-	// Don't lose an existing provider, like Mist or Metamask
-	web3 = new Web3(web3.currentProvider);
+    // Don't lose an existing provider, like Mist or Metamask
+    web3 = new Web3(web3.currentProvider);
 } else {
-	// set the provider you want from Web3.providers
-	web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"));
+    // set the provider you want from Web3.providers
+    web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"));
 }
 web3.eth.getCoinbase(function(err, coinbase) {
-	if (err) {
-		console.error(err);
-	} else {
-		console.log("Coinbase: " + coinbase);
-	}
+    if (err) {
+        console.error(err);
+    } else {
+        console.log("Coinbase: " + coinbase);
+    }
 });
 
 // Your deployed address changes every time you deploy.
@@ -20,64 +20,64 @@ const faucetInstance = faucetContractFactory.at(faucetAddress);
 
 // Query eth for balance
 web3.eth.getBalance(faucetAddress, function(err, balance) {
-	if (err) {
-		console.error(err);
-	} else {
-		console.log("Contract balance: " + balance);
-	}
+    if (err) {
+        console.error(err);
+    } else {
+        console.log("Contract balance: " + balance);
+    }
 });
 
 // Query the contract directly
 faucetInstance.getBalance.call(function(err, balance) {
-	if (err) {
-		console.error(err);
-	} else {
-		console.log("Faucet balance: " + balance);
-	}
+    if (err) {
+        console.error(err);
+    } else {
+        console.log("Faucet balance: " + balance);
+    }
 });
 
 function topUp() {
-	web3.eth.getCoinbase(function(err, coinbase) {
-		if (err) {
-			console.error(err);
-		} else {
-			web3.eth.sendTransaction({ 
-				from: coinbase, 
-				to: faucetAddress,
-				value: web3.toWei(1, "ether") 
-			}, function(err, txn) {
-				if (err) {
-					console.error(err);
-				} else {
-					console.log("topUp txn: " + txn);
-				}
-			});
-		}
-	});
+    web3.eth.getCoinbase(function(err, coinbase) {
+        if (err) {
+            console.error(err);
+        } else {
+            web3.eth.sendTransaction({ 
+                from: coinbase, 
+                to: faucetAddress,
+                value: web3.toWei(1, "ether") 
+            }, function(err, txn) {
+                if (err) {
+                    console.error(err);
+                } else {
+                    console.log("topUp txn: " + txn);
+                }
+            });
+        }
+    });
 }
 
 function sendWei() {
-	web3.eth.getCoinbase(function(err, coinbase) {
-		if (err) {
-			console.error(err);
-		} else {
-			web3.eth.getAccounts(function(err, accounts) {
-				if (err) {
-					console.error(err);
-				} else {
-					const targetAccount = accounts[1];      
-					faucetInstance.sendWei(
-						targetAccount,
-						{ from: coinbase },
-						function(err, txn) {
-							if (err) {
-								console.error(err);
-							} else {
-								console.log("sendWei txn: " + txn);
-							}
-						});
-				}
-			});
-		}
-	});
+    web3.eth.getCoinbase(function(err, coinbase) {
+        if (err) {
+            console.error(err);
+        } else {
+            web3.eth.getAccounts(function(err, accounts) {
+                if (err) {
+                    console.error(err);
+                } else {
+                    const targetAccount = accounts[1];      
+                    faucetInstance.sendWei(
+                        targetAccount,
+                        { from: coinbase },
+                        function(err, txn) {
+                            if (err) {
+                                console.error(err);
+                            } else {
+                                console.log("sendWei txn: " + txn);
+                            }
+                        });
+                }
+            });
+        }
+    });
 }
